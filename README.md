9d1ce715da9374755718b1ba69cf72c5b6741718
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-06-17 14:30:59 +0100

    work

---
+++ README.md
@@ -0,0 +1,8 @@
+# 1. 创建虚拟环境（你可以把 venv 换成你想要的文件夹名）
+python3 -m venv venv
+
+# 2. 激活虚拟环境
+source venv/bin/activate
+
+# 3. 安装 Sphinx 和主题
+pip install sphinx sphinx-pdj-theme
--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_pdj_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+import sphinx_pdj_theme
+html_theme = 'sphinx_pdj_theme'
